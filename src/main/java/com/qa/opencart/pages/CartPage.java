9f557e7b5661a37a6ed919ac4155411098c00a69
Manish Upadhyay <manish.upadhyay27@gmail.com>
2022-07-20 13:36:35 +0530

    cartpage

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -5,8 +5,13 @@ public class CartPage {
 	public void printcart() {
 	
 		
-		System.out.println("cart items");
-		System.out.println("cart ADDED");
+		System.out.println("Changes for local merge");
+		
+		System.out.println("Changes for local merge");
+		
+		
+		System.out.println("Changes for local merge");
+		
 		
 		System.out.println("Changes for local merge");
 	}
