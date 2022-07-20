7ff7b8e3ce36f3084710ac05d1f3f4e1474ba51b
Manish Upadhyay <manish.upadhyay27@gmail.com>
2022-07-20 12:56:42 +0530

    updated cartpage

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,10 +1,16 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 	
 	public void printcart() {
+		
+		private By item= By.id("item");
 		System.out.println("cart items");
 		System.out.println("cart ADDED");
+		
+		System.out.println(+item);
 	}
 
 }
